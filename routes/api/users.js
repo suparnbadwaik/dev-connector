8a5b9dc827b2490bb5d004684db270778102aecc
suparn.badwaik <suparn.badwaik@gmail.com>
2019-05-29 20:25:06 -0700

    JWT returned

--- routes/api/users.js
+++ routes/api/users.js
@@ -6,6 +6,8 @@ const User = require("../../models/User");
 const gravatar = require("gravatar");
 const bcrypt = require("bcrypt");
 const router = express.Router();
+const config = require("config");
+const jwt = require("jsonwebtoken");
 
 // @route   /api/users/
 // @access  public
@@ -45,9 +47,9 @@ router.post(
 
       // Get user gravatar
       const avatar = gravatar.url(email, {
-        s: "200",
-        r: "pg",
-        d: "mm"
+        s: "200", // size
+        r: "pg", // rating
+        d: "mm" // default
       });
 
       // Encrypt the password using bcrypt
@@ -57,11 +59,32 @@ router.post(
       const salt = await bcrypt.genSalt(10); // the more rounds, the more secure, the more slower
       user.password = await bcrypt.hash(password, salt);
 
+      // save user in the database
       await user.save();
 
       // Return web token to get the users logged in right away as soon as they register
+      const payload = {
+        user: {
+          id: user.id
+        }
+      };
 
-      res.send("User registered !!!");
+      // jwt.sign(
+      //   payload,
+      //   config.get("jwtSecret"),
+      //   { expiresIn: 36000 },
+      //   (err, token) => {
+      //     if (err) throw err;
+      //     console.log(token);
+      //     res.json({ token });
+      //     res.send(token);
+      //   }
+      // );
+
+      let token = jwt.sign(payload, config.get("jwtSecret"), {
+        expiresIn: 36000
+      });
+      res.status(200).send({ token });
     } catch (err) {
       res.status(500).send("This is an internal server error !!!");
     }
